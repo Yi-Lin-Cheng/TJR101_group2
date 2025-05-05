ee957bda57160fd51035cac3deb35374439d9cdc
James <gn01637466@gmail.com>
2025-05-05 12:58:13 +0800

    refactor: simplify main scraping loop structure and rename

--- src/attractions/spot05_extract_google_info.py
+++ src/attractions/spot05_extract_google_info.py
@@ -13,8 +13,37 @@ from selenium.webdriver.support import expected_conditions as EC
 from utils import web_open
 
 
-def spot05_extract_goolge_info():
-    file_path = Path("data", "spot")
+def spot05_extract_google_info(url, driver, wait):
+    try:
+        driver.get(url)
+        time.sleep(random.uniform(2, 4))
+        wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "img[decoding='async']"))
+        )
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, "html.parser")
+        b_hours_tag = soup.select_one(".eK4R0e.fontBodyMedium")
+        b_hours = b_hours_tag.text if b_hours_tag else ""
+        pic_url = soup.select_one("img[decoding='async']")["src"]
+        rate_comm_tag = soup.select_one(".F7nice")
+        if rate_comm_tag and rate_comm_tag.text != "":
+            rate = rate_comm_tag.select_one("span[aria-hidden]").text
+            comm = re.sub(
+                r"\(|,|\)", "", rate_comm_tag.select("span[aria-label]")[1].text
+            )
+        return {
+            "b_hours": b_hours,
+            "rate": rate,
+            "pic_url": pic_url,
+            "comm": comm,
+            "error": False,
+        }
+    except Exception as err:
+        time.sleep(random.uniform(5, 10))
+        return {"b_hours": "", "rate": None, "pic_url": "", "comm": None, "error": err}
+
+
+def load_data(file_path):
     if (file_path / "spot05_extract_googlemap_progress.csv").exists():
         print("發現進度檔案，從中斷處繼續")
         data = pd.read_csv(
@@ -60,7 +89,33 @@ def spot05_extract_goolge_info():
                 engine="python",
             )
             data = pd.concat([data1, data], ignore_index=True)
+        return data
 
+
+def save_data(file_path, data, err_log):
+    data.to_csv(
+        file_path / "spot05_extract_googlemap.csv",
+        encoding="utf-8",
+        header=True,
+        index=False,
+    )
+    data.to_csv(
+        file_path / "spot05_extract_googlemap_progress.csv",
+        encoding="utf-8",
+        header=True,
+        index=False,
+    )
+    with open(
+        file_path / "spot05_extract_googlemap_err_log.txt",
+        "a",
+        encoding="utf-8",
+    ) as f:
+        f.write(err_log)
+
+
+def main():
+    file_path = Path("data", "spot")
+    data = load_data(file_path)
     if data["update_time"].notna().any():
         condition = data["update_time"] <= datetime.today() - timedelta(hours=20)
         start_idx = data[condition].index.min()
@@ -69,7 +124,8 @@ def spot05_extract_goolge_info():
             return
     else:
         start_idx = 0
-    data.loc[data["create_time"].isna(), "create_time"] = datetime.now()
+    now = datetime.now().replace(microsecond=0)
+    data.loc[data["create_time"].isna(), "create_time"] = now
     gmaps_url_list = data["gmaps_url"].tolist()
 
     for i in range(start_idx, len(gmaps_url_list)):
@@ -83,44 +139,23 @@ def spot05_extract_goolge_info():
             comm_list = []
             update_time_list = []
             err_log = ""
-        try:
-            url = gmaps_url_list[i]
-            driver.get(url)
-            time.sleep(random.uniform(2, 4))
-            wait.until(
-                EC.presence_of_element_located(
-                    (By.CSS_SELECTOR, "img[decoding='async']")
-                )
-            )
-            page_source = driver.page_source
-            soup = BeautifulSoup(page_source, "html.parser")
-            b_hours_tag = soup.select_one(".eK4R0e.fontBodyMedium")
-            b_hours = b_hours_tag.text if b_hours_tag else ""
-            pic_url = soup.select_one("img[decoding='async']")["src"]
-            rate_comm_tag = soup.select_one(".F7nice")
-            if rate_comm_tag and rate_comm_tag.text != "":
-                rate = rate_comm_tag.select_one("span[aria-hidden]").text
-                comm = re.sub(
-                    r"\(|,|\)", "", rate_comm_tag.select("span[aria-label]")[1].text
-                )
-            update_time = datetime.now()
-            b_hours_list.append(b_hours)
-            rate_list.append(rate)
-            pic_url_list.append(pic_url)
-            comm_list.append(comm)
-            update_time_list.append(update_time)
+        url = gmaps_url_list[i]
+        result = spot05_extract_google_info(url, driver, wait)
+        if not result["error"]:
             print(f"第{i+1}筆完成")
-        except Exception as err:
-            err_msg = f"第{i+1}筆資料 {url} 出現錯誤"
+            update_time = now
+        else:
+            err_msg = f"{datetime.now()}第{i+1}筆 {url} 出現錯誤"
+            print(err_msg)
             err_log += err_msg + "\n"
-            b_hours_list.append("")
-            rate_list.append(None)
-            pic_url_list.append("")
-            comm_list.append(None)
-            update_time_list.append(pd.NaT)
-            time.sleep(random.uniform(5, 10))
-            print(err_msg, "\n", err)
-
+            update_time = (
+                data.at[i, "update_time"] if data.at[i, "update_time"] else pd.NaT
+            )
+        b_hours_list.append(result["b_hours"])
+        rate_list.append(result["rate"])
+        pic_url_list.append(result["pic_url"])
+        comm_list.append(result["comm"])
+        update_time_list.append(update_time)
         if ((i + 1) % 50 == 0) or i + 1 == len(gmaps_url_list):
             end_idx = i + 1
             start_idx = end_idx - len(b_hours_list)
@@ -129,24 +164,7 @@ def spot05_extract_goolge_info():
             data.loc[start_idx : end_idx - 1, "pic_url"] = pic_url_list
             data.loc[start_idx : end_idx - 1, "comm"] = comm_list
             data.loc[start_idx : end_idx - 1, "update_time"] = update_time_list
-            data.to_csv(
-                file_path / "spot05_extract_googlemap.csv",
-                encoding="utf-8",
-                header=True,
-                index=False,
-            )
-            data.to_csv(
-                file_path / "spot05__extract_googlemap_progress.csv",
-                encoding="utf-8",
-                header=True,
-                index=False,
-            )
-            with open(
-                file_path / "spot05__extract_googlemap_err_log.txt",
-                "a",
-                encoding="utf-8",
-            ) as f:
-                f.write(err_log)
+            save_data(file_path, data, err_log)
             print(f"第{i+1}筆儲存完成")
             time.sleep(random.uniform(4, 6))
             driver.quit()
@@ -156,5 +174,5 @@ def spot05_extract_goolge_info():
         (file_path / "spot05_extract_googlemap_progress.csv").unlink()
     print("已完成全部資料，進度檔案已刪除")
 
-
-spot05_extract_goolge_info()
+if __name__ == "__main__":
+    main()
