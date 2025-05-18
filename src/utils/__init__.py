f63272605b76ed105cd9127b5a75ca3f8395a829
James <gn01637466@gmail.com>
2025-05-18 11:43:29 +0800

    test: CI/CD

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
+from .airflow_notify import line_notify_failure
 from .encoding_transform import encoding_transform
 from .mysql_connect import close_connection, get_connection
 from .to_half_width import to_half_width
